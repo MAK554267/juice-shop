24420a3ca71a4ba6dbf4932191ede7db233a3ada
mak55_18 <azfar.waqas6678@gmail.com>
2025-11-26 09:43:36 +0500

    Refine search criteria for product queries
    Updated search criteria to allow only 'apple' or 'orange' searches with a proper error response.

--- data/static/codefixes/unionSqlInjectionChallenge_3.ts
+++ data/static/codefixes/unionSqlInjectionChallenge_3.ts
@@ -2,11 +2,10 @@ export function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    // only allow apple or orange related searches
-    if (!criteria.startsWith("apple") || !criteria.startsWith("orange")) {
-      res.status(400).send()
-      return
-    }
+      // Allow only apple or orange searches
+      if (!(criteria.startsWith("apple") || criteria.startsWith("orange"))) {
+        return res.status(400).json({ error: "Invalid search filter" });
+      }
     models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`)
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
